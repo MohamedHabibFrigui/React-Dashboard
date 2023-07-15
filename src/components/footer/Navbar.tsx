f585e1449822b91e538f3e3404469448e5f70b73
Mohamed Habib Frigui <mohamedhabibfrigui9@gmail.com>
2023-07-15 10:41:30 +0100

    Add Navbar, Footer and Menu

--- src/components/footer/Navbar.tsx
+++ src/components/footer/Navbar.tsx
@@ -1,7 +1,28 @@
 import "./navbar.scss";
 
 const Navbar = () => {
-  return <div className="navbar">Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <img src="logo.svg" alt="logo" />
+        <span>admin</span>
+      </div>
+      <div className="icons">
+        <img src="/search.svg" alt="icon" className="icon" />
+        <img src="/app.svg" alt="icon" className="icon" />
+        <img src="/expand.svg" alt="icon" className="icon" />
+        <div className="notification">
+          <img src="/notifications.svg" alt="icon" />
+          <span>1</span>
+        </div>
+        <div className="user">
+          <img src="/userImg.jpeg" alt="user" />
+          <span>Jane</span>
+        </div>
+        <img src="/settings.svg" alt="icon" className="icon" />
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
--- src/components/navbar/Footer.tsx
+++ src/components/navbar/Footer.tsx
@@ -1,7 +1,12 @@
 import "./footer.scss";
 
 const Footer = () => {
-  return <div className="footer">Footer</div>;
+  return (
+    <div className="footer">
+      <span>admin</span>
+      <span>&copy; Admin Dashboard</span>
+    </div>
+  );
 };
 
 export default Footer;
