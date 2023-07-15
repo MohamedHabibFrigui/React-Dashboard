fba402b178354258cc8d672b31fc8442c6ae6388
Mohamed Habib Frigui <mohamedhabibfrigui9@gmail.com>
2023-07-15 17:47:03 +0100

    Add the Products page

--- src/pages/products/Products.tsx
+++ src/pages/products/Products.tsx
@@ -1,7 +1,70 @@
+import Add from "../../components/add/Add";
+import DataTable from "../../components/dataTable/DataTable";
+import { products } from "../../data";
 import "./products.scss";
+import { GridColDef } from "@mui/x-data-grid";
+import { useState } from "react";
+
+const columns: GridColDef[] = [
+  { field: "id", headerName: "ID", width: 40 },
+  {
+    field: "img",
+    headerName: "Image",
+    width: 90,
+    renderCell: (params) => {
+      return <img src={params.row.img || "/noavatar.png"} alt="" />;
+    },
+  },
+  {
+    field: "title",
+    type: "string",
+    headerName: "Title",
+    width: 250,
+  },
+  {
+    field: "color",
+    type: "string",
+    headerName: "Color",
+    width: 90,
+  },
+  {
+    field: "price",
+    type: "string",
+    headerName: "Price",
+    width: 120,
+  },
+  {
+    field: "producer",
+    headerName: "Producer",
+    type: "string",
+    width: 120,
+  },
+  {
+    field: "createdAt",
+    headerName: "Created At",
+    width: 100,
+    type: "string",
+  },
+  {
+    field: "inStock",
+    headerName: "In Stock",
+    width: 150,
+    type: "boolean",
+  },
+];
 
 const Products = () => {
-  return <div className="products">Products</div>;
+  const [open, setOpen] = useState(false);
+  return (
+    <div className="products">
+      <div className="info">
+        <h1>Users</h1>
+        <button onClick={() => setOpen(true)}>Add New Product</button>
+      </div>
+      <DataTable slug="products" columns={columns} rows={products} />
+      {open && <Add slug="product" columns={columns} setOpen={setOpen} />}
+    </div>
+  );
 };
 
 export default Products;
